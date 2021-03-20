da3b45e4152ef62fd341dbd9c766e02cbcbee461
giovannig37 <giovannirodriguez761@gmail.com>
2021-03-20 01:58:21 -0400

    week1

---
+++ index.js
@@ -0,0 +1,15 @@
+var itemPrice = 40;
+var amountOfMoneyInWallet = 80;
+var numberOfFriends = 10;
+var ageInYears = 22;
+var firstName = "Giovanni" ;
+var lastName = "Rodriguez" ;
+var middleInitial = "B" ;
+
+let afterPurchase = (amountOfMoneyInWallet - itemPrice);
+
+console.log("After purchase: $" + afterPurchase);
+
+console.log("Ive made this many friends every year of my life: " + numberOfFriends / ageInYears);
+
+console.log("My Name is: " + firstName, middleInitial, lastName); 
